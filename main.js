e073db554edbab93f294408f4c025e84a52593a5
Kate Gordon <kategordon.code@gmail.com>
2019-09-25 11:48:43 -0400

    Rewrote program producing dynamic elements in JS

--- main.js
+++ main.js
@@ -1,97 +1,89 @@
 
 
-const currLocation = document.getElementById("location"),
-        currTemp = document.getElementById("temp"),
-        wind = document.getElementById("windSpeed"),
-        sunriseTime = document.getElementById("sunrise"),
-        sunsetTime = document.getElementById("sunset"),
-        currentIcon = document.getElementById("icon"); 
+const weatherWrapper = document.querySelector("#weatherWrapper");
+const cityForm = document.querySelector("#cityForm");
 
-function currIcon(){   
-    return fetch('https://api.openweathermap.org/data/2.5/weather?q=Atlanta,US&appid=2f4580c1da2a1471787ee4c356181fd1') 
-    .then(function(response){
-        return response.json(); 
-    })
-    .then(function(data){
-        const iconCode = data.weather[0].icon; 
-        const iconUrl =`http://openweathermap.org/img/w/${iconCode}.png`; 
-        currentIcon.setAttribute("src", iconUrl);
-    }); 
-};    
+const formButton = cityForm.querySelector("button");
 
+formButton.addEventListener("click", function(e) {
+    e.preventDefault();
+});
 
+function getFormData() {}
 
-
-function updateLocationName(){
-    return fetch('https://api.openweathermap.org/data/2.5/weather?q=Atlanta,US&appid=2f4580c1da2a1471787ee4c356181fd1') 
-    .then(function(response){
-        return response.json(); 
-    })
-    .then(function(data){
-        currLocation.innerHTML = "Location: " + data.name; 
-    });
-    
-    
-};
-function updateWind(){
-    return fetch('https://api.openweathermap.org/data/2.5/weather?q=Atlanta,US&appid=2f4580c1da2a1471787ee4c356181fd1') 
-    .then(function(response){
-        return response.json(); 
-    })
-    .then(function(data){
-        wind.innerHTML = "Windspeed is " + data.wind.speed; 
-    });
-    
-    
-};
-function updateTemp(){
-    return fetch('https://api.openweathermap.org/data/2.5/weather?q=Atlanta,US&appid=2f4580c1da2a1471787ee4c356181fd1') 
-    .then(function(response){
-        return response.json(); 
-    })
-    .then(function(data){
-        tempKel = data.main.temp 
-        tempF = (((tempKel - 273.15)*9)/5) + 32;
-        currTemp.innerHTML = "Temperature: " + Math.round(tempF); 
-    });
-    
-    
-};
-
-function currSunrise(){
-    return fetch('https://api.openweathermap.org/data/2.5/weather?q=Atlanta,US&appid=2f4580c1da2a1471787ee4c356181fd1') 
-    .then(function(response){
-        return response.json(); 
-    })
-    .then(function(data){
-        rise = data.sys.sunrise
-        // riseDate = Date.prototype.getDate(rise)
-        riseTime = new Date(rise * 1000)
-        sunriseTime.innerHTML = "Sunrise: " + riseTime; 
-    });
-    
-    
-};
-
-function currSunset(){
-    return fetch('https://api.openweathermap.org/data/2.5/weather?q=Atlanta,US&appid=2f4580c1da2a1471787ee4c356181fd1') 
-    .then(function(response){
-        return response.json(); 
-    })
-    .then(function(data){
-        newTime = data.sys.sunset;
-        converted = new Date(newTime * 1000);
-        riseDay = converted.getDay();
+function addLocationName(name) {
+    const locationName = document.createElement("p"); 
+    locationName.innerHTML = `Location: ${name}`; 
+    weatherWrapper.append(locationName);
+}
         
-        sunsetTime.innerHTML = "Sunset: " + riseDay; 
-    });
+function addTemp(temp) {
+    const currentTemp = document.createElement("p");
+    currentTemp.innerHTML = `Temperature: ${temp + '&#176'}`; 
+    weatherWrapper.append(currentTemp);
+}
+
+function addConditions(conditions) {
+    const conditionIcon = document.createElement("IMG");
+    const iconCode = conditions; 
+    const iconUrl =`http://openweathermap.org/img/w/${iconCode}.png`; 
+    conditionIcon.setAttribute('src', iconUrl);
+    weatherWrapper.append(conditionIcon); 
+}
+
+function addWind(windSpeed) {
+    const currWind = document.createElement("p"); 
+    currWind.innerHTML = `Current Wind Speed: ${windSpeed} mph`;
+    weatherWrapper.append(currWind);
+}
+
+function addSunInfo(sunrise, sunset) {
+    const sunSet = document.createElement("p"); 
+    const sunRise = document.createElement("p"); 
+    sunRise.innerHTML = `Sunrise: ${sunrise}`; 
+    sunSet.innerHTML = `Sunset: ${sunset}`; 
+    weatherWrapper.append(sunSet);
+    weatherWrapper.append(sunRise); 
+}
+
+function addMap(lat, lon) {
+    const ifrm = document.createElement('iframe'); 
+    const mapUrl = `http://maps.google.com/maps?q=${lat},${lon}&output=embed`;
+    ifrm.setAttribute('src', mapUrl); 
+    weatherWrapper.append(ifrm);
+}
+
+function getWeather(location) {
+    const selectWrapper = document.querySelector("#selectWrapper")
+    const categoryList = document.createElement("select"); 
+
+    get(`https://api.openweathermap.org/data/2.5/weather?q=${location},US&units=imperial&appid=2f4580c1da2a1471787ee4c356181fd1`).then(function(response){
+        addConditions(response.weather[0].icon); 
+        addLocationName(response.name); 
+        addMap(response.coord.lat, response.coord.lon); 
+        addTemp(Math.round(response.main.temp));
+        addWind(response.wind.speed);
+        addSunInfo(moment.unix(response.sys.sunrise).format("MM/DD/YYYY h:mm A"), moment.unix(response.sys.sunset).format("MM/DD/YYYY h:mm A")); 
+        
+    })
     
+    weatherWrapper.append(categoryList); 
     
-};
-currIcon()
-updateWind();
-updateTemp();
-updateLocationName();
-currSunrise(); 
-currSunset();
+}
+
+
+
+getWeather("Atlanta"); 
+
+
+
+
+
+
+
+
+
+    
+
+
 
